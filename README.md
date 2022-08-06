98384bc37bdd6752ad9c8fa04770876eb9025409
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2022-08-06 13:05:44 +0530

    Added Synonym Pair checker and replacer(Completed Step 3)

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ Implementation of semantic cosine similarity from the paper: https://www.researc
 ### To-Do List
 - [x] - Dimension Equalization
 - [ ] - Check for Hypernym Hyponym Pair
-- [ ] - Check for Synonym Pairs
+- [x] - Check for Synonym Pairs and replace it
 - [ ] - Recalibrate Dimension Values
+
+#### How does Synonym Pair work?
+For finding the Synonyms, I approached the solution by going through https://thesaurus.com for finding synoyms then, picking up only the relevant ones which are needed and present in s1
+Then I replaced the words
--- enhancement.py
+++ enhancement.py
@@ -1,26 +1,6 @@
 import numpy as np
 from tqdm import tqdm
 
-with open('Dataset/glove.6B.50d.txt', 'r') as file:
-    data = file.readlines()
-
-print("Data loaded")
-
-# No clue why this exists -> forgot to document it
-for i in range(len(data)):
-    data[i] = data[i][:-1]
-
-print("Data split")
-
-# Add all codes to a dictionary
-data_dict = dict()
-
-for i in tqdm(range(len(data))):
-
-    split_data = data[i].split()
-    data_dict[split_data[0]] = np.array(split_data[1:]).astype('float64')
-
-print("Data dictionarized")
 
 class Enhancement:
     """
@@ -30,6 +10,29 @@ class Enhancement:
     def __init__(self, s1, s2):
         self.s1 = s1.lower().split()
         self.s2 = s2.lower().split()
+
+    def dictionaryInitializer(self):
+        with open('Dataset/glove.6B.50d.txt', 'r') as file:
+            data = file.readlines()
+
+        print("Data loaded")
+
+        # No clue why this exists -> forgot to document it
+        for i in range(len(data)):
+            data[i] = data[i][:-1]
+
+        print("Data split")
+
+        # Add all codes to a dictionary
+        self.data_dict = dict()
+
+        for i in tqdm(range(len(data))):
+            split_data = data[i].split()
+            self.data_dict[split_data[0]] = np.array(split_data[1:]).astype('float64')
+
+        print("Data dictionarized")
+
+
 
     def dimensionEqualization(self):
         """
@@ -52,15 +55,61 @@ class Enhancement:
                 if self.s1[i] not in self.s2:
                     self.s2.insert(i, (self.s1[i], 0))
 
-    def norm(self, s):
-        return np.sqrt(np.sum((data_dict[s]**2)))
 
+    # Get Synonym of the word
+    def getSynonym(self, word):
+
+        import requests
+        from bs4 import BeautifulSoup
+
+        def synonyms(term):
+            response = requests.get('https://www.thesaurus.com/browse/{}'.format(term))
+            soup = BeautifulSoup(response.text, 'lxml')
+            soup.find('section', {'class': 'css-17ofzyv e1ccqdb60'})
+            return [span.text for span in soup.findAll('a', {'class': 'css-1kg1yv8 eh475bn0'})] # 'css-1gyuw4i eh475bn0' for less relevant synonyms
+
+        L = []
+        L_synoyms = [x.replace(' ', '') for x in synonyms(word)]
+
+
+        for i in L_synoyms:
+            if i not in self.s1 and i not in self.s2:
+                continue
+            else:
+                L.append(i)
+
+        return L
+
+    def replaceWords(self):
+        """
+        Replace words in the two strings
+        """
+
+        for i in range(len(self.s1)):
+
+            if self.getSynonym(self.s1[i]) == []:
+                continue
+            else:
+                self.s2[self.s2.index(self.getSynonym(self.s1[i])[0])] = self.s1[i]
+
+        for i in range(len(self.s2)):
+
+            if self.getSynonym(self.s2[i]) == []:
+                continue
+            else:
+                self.s1[self.s1.index(self.getSynonym(self.s2[i])[0])] = self.s2[i]
+
+    # Norm to find the vector of the work/string
+    def norm(self, s):
+        return np.sqrt(np.sum((self.data_dict[s]**2)))
 
     def cosine_similarity(self):
         """
         Cosine similarity of the two strings
         """
 
+        self.dictionaryInitializer()
+        self.replaceWords()
         # Dimension equalization
         self.dimensionEqualization()
 
@@ -72,7 +121,6 @@ class Enhancement:
 
             if type(self.s1[i]) is tuple or type(self.s2[i]) is tuple:
                 continue
-
             numerator += self.norm(self.s1[i]) * self.norm(self.s2[i])
 
         denominator1, denominator2 = 0, 0
@@ -91,14 +139,10 @@ class Enhancement:
 
         denominator = np.sqrt(denominator1) * np.sqrt(denominator2)
 
-
         return numerator / denominator
 
 
-obj = Enhancement('This table is Stupid', 'This desk is stupid')
-
+obj = Enhancement('This desk is not stupid', 'This table is stupid')
 print(obj.cosine_similarity())
-
-
-
+# print(obj.replaceWords())
 
